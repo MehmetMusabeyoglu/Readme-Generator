5fd2ac61166fc908804148f023be477ab76e2ca2
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-10-21 17:59:43 -0700

    Edited const questions

--- index.js
+++ index.js
@@ -4,15 +4,6 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = ["What is your GitHub username?",
-    "What is your e-mail address?",
-    "What is your project's name?",
-    "What is your project's description?",
-    "Which of the license options would you like to select for your project?",
-    "What command should be run to install dependencies?",
-    "What command should be run to run tests?",
-    "What does the user need to know about using the repo?",
-    "What does the user need to know about contributing to the repo?"];
 
 const licenseChoices = ["Apache 2.0 License",
     "Boost Software License 1.0",
@@ -33,6 +24,58 @@ const licenseChoices = ["Apache 2.0 License",
     "None"];
 
 
+const questions = [
+    {
+        type: "input",
+        name: "userName",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "userEmail",
+        message: "What is your e-mail address?",
+    },
+    {
+        type: "input",
+        name: "projectName",
+        message: "What is your project's name?",
+    },
+    {
+        type: "input",
+        name: "projectDescription",
+        message: "What is your project's description?",
+    },
+    {
+        type: "list",
+        name: "projectLicense",
+        message: "Which of the license options would you like to select for your project?",
+        choices: licenseChoices,
+    },
+    {
+        type: "input",
+        name: "dependenciesCommand",
+        message: "What command should be run to install dependencies?",
+    },
+    {
+        type: "input",
+        name: "testCommand",
+        message: "What command should be run to run tests?",
+    },
+    {
+        type: "input",
+        name: "usingRepo",
+        message: "What does the user need to know about using the repo?",
+    },
+    {
+        type: "input",
+        name: "contributingRepo",
+        message: "What does the user need to know about contributing to the repo?",
+    }];
+
+
+
+
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, userInputs) {
     fs.writeFile(fileName, generateMarkdown(userInputs), (err) =>
@@ -41,60 +84,13 @@ function writeToFile(fileName, userInputs) {
 }
 
 // TODO: Create a function to initialize app
-function init() { 
+function init() {
     inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'userName',
-            message: questions[0],
-        },
-        {
-            type: 'input',
-            name: 'userEmail',
-            message: questions[1],
-        },
-        {
-            type: 'input',
-            name: 'projectName',
-            message: questions[2],
-        },
-        {
-            type: 'input',
-            name: 'projectDescription',
-            message: questions[3],
-        },
-        {
-            type: 'list',
-            name: 'projectLicense',
-            message: questions[4],
-            choices: licenseChoices,
-        },
-        {
-            type: 'input',
-            name: 'dependenciesCommand',
-            message: questions[5],
-        },
-        {
-            type: 'input',
-            name: 'testCommand',
-            message: questions[6],
-        },
-        {
-            type: 'input',
-            name: 'usingRepo',
-            message: questions[7],
-        },
-        {
-            type: 'input',
-            name: 'contributingRepo',
-            message: questions[8],
-        }
-    ])
-    .then((userInputs) => {
-        console.log(userInputs);
-        writeToFile('generated/Generated-Readme.md', userInputs);
-    });
+        .prompt(questions)
+        .then((userInputs) => {
+            console.log(userInputs);
+            writeToFile('generated/Generated-Readme.md', userInputs);
+        });
 }
 
 // Function call to initialize app
